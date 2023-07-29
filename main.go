f8429a5bc52aeceb776134db0c19b3d50466eaf2
aymene01 <aymene01@hotmail.fr>
2023-07-29 10:48:16 +0200

    setup handlers + http servers

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/hey-microservices/handlers"
 )
 
 func main() {
-	fmt.Println("hey microservices")
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
