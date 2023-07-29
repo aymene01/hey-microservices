ddb68c861b141bd3ae52eaf684d74c7ec8b3d593
aymene01 <aymene01@hotmail.fr>
2023-07-29 11:12:10 +0200

    enable graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/hey-microservices/handlers"
@@ -28,5 +30,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	s.Shutdown(tc)
+
 }
